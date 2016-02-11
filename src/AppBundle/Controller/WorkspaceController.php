235d65b79b70e02db9bb348884db6ad6c8e289c5
Sohail <info@soolan.com>
2016-02-11 17:28:07 +0800

    feature (cest): add a cest functional test for workspace show controller

---
+++ src/AppBundle/Controller/WorkspaceController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class WorkspaceController extends Controller
+{
+    /**
+     * @Route("/workspace/{name}", name="workspace_show")
+     * @param $workspace
+     * @return Response
+     */
+    public function showAction($name)
+    {
+        // ToDO: find workspace projects via given workspace name
+        return $this->render('workspace/show.html.twig', array('project' => 'Symfony book'));
+    }
+}

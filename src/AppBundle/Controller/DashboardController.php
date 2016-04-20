9924f3ef652816674b2ad8a774547ff08885e286
Sohail <info@soolan.com>
2016-04-20 12:42:12 +0800

    feature(cache): add cache samples

--- src/AppBundle/Controller/DashboardController.php
+++ src/AppBundle/Controller/DashboardController.php
@@ -7,23 +7,27 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * @Cache(smaxage="1800", public="true")
+ * @Cache(expires="600")
+ */
 class DashboardController extends Controller
 {
     /**
-     * @Cache(expires="next Friday")
+     * @Cache(ETag="userProjects ~ finishedTasks")
      * @Route("/dashboard", name="dashboard_index")
      */
     public function indexAction()
     {
         $uId = $this->getUser()->getId();
-//        $util = $this->get('mava_util');
-        $userProjects = 6; //$util->getUserProjects($uId);
-        $currentTasks= 7; //$util->getUserTasks($uId, 'in progress');
-
+        $util = $this->get('mava_util');
+        $userProjects = $util->getUserProjects($uId);
+        $currentTasks = $util->getUserTasks($uId, 'in progress');
+        
         return $this->render(
             ':dashboard:index.html.twig', array(
             'currentTasks'  => $currentTasks,
-            'userProjects'  => $userProjects
-        ));
+            'userProjects'  => $userProjects)
+        );
     }
 }
